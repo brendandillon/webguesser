cb53a66f0a5b8e4b09b157f5103a8a050fba8982
Brendan Dillon <br.lyle.dillon@gmail.com>
2016-08-06 17:38:15 -0600

    I4: colorized output

--- web_guesser.rb
+++ web_guesser.rb
@@ -5,20 +5,25 @@ SECRET_NUMBER = rand(101)
 
 get '/' do 
   user_guess = params[:guess].to_i
-  message = check_guess(user_guess)
-  erb :index, :locals => {:number => SECRET_NUMBER, :message => message}
+  check_guess(user_guess)
+  erb :index, :locals => {:message => @message, :back_color => @color}
 end
 
 def check_guess(user_guess)
   if user_guess > (SECRET_NUMBER + 5)
-    "Way too high!"
+    @message = "Way too high!"
+    @color = "f00"
   elsif user_guess > SECRET_NUMBER
-    "Too high!"
+    @message = "Too high!"
+    @color = "f88"
   elsif user_guess < (SECRET_NUMBER - 5)
-    "Way too low!"
+    @message = "Way too low!"
+    @color = "f00"
   elsif user_guess < SECRET_NUMBER
-    "Too low!"
+    @message = "Too low!"
+    @color = "f88"
   else
-    "You got it right!<br>The SECRET NUMBER is #{SECRET_NUMBER}"
+    @message = "You got it right!<br>The SECRET NUMBER is #{SECRET_NUMBER}"
+    @color = "0c0"
   end
 end
